33ac139a6787d16716e8f291ef42eb6df467dd16
Mark Eschbach <meschbach@gmail.com>
2014-04-14 20:25:29 -0700

    Passing system status test

---
+++ service/service.js
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2014 Mark Eschbach
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+var express = require( "express" );
+var connect = require( "connect" );
+
+function redirect( url ){
+	return function( request, response ){
+		response.writeHead( 302, {
+			'Location' : url
+		});
+		response.end();
+	}
+}
+
+function onomate_express_assembly( application, a_context ){
+	var context = a_context ? a_context : ""; 
+	application.use( context, connect.static( "browser/" ) );
+	application.get( "/status", redirect( "/status.html" ) ); 
+}
+
+var express_application = express();
+express_application.use( connect.logger('tiny') );
+
+onomate_express_assembly( express_application );
+
+var port = 9000;
+express_application.listen( port, function(){
+	console.log( "Running on ", port );
+});
+
