78c6f58647d1f116f40269836dc5813062965baa
Mark Eschbach <meschbach@gmail.com>
2014-05-16 19:18:02 -0700

    Now using db-migrate for database schema management

--- migrations/pdns-seed.sql
+++ migrations/pdns-seed.sql
@@ -1,26 +1,18 @@
 -- Copyright 2014 Mark eschbach
--- 
+--
 -- Licensed under the Apache License, Version 2.0 (the "License");
 -- you may not use this file except in compliance with the License.
 -- You may obtain a copy of the License at
--- 
+--
 --     http://www.apache.org/licenses/LICENSE-2.0
--- 
+--
 -- Unless required by applicable law or agreed to in writing, software
 -- distributed under the License is distributed on an "AS IS" BASIS,
 -- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
---
--- PowerDNS Example Schema
--- http://doc.powerdns.com/html/generic-mypgsql-backends.html @ 2014-04-05
---
-BEGIN; 
-INSERT INTO schema_migrations ( version, description ) SELECT 1, 'PowerDNS default schema (no DNSSEC)' WHERE NOT EXISTS( SELECT version FROM schema_migrations WHERE version = 1 ); 
-INSERT INTO schema_migration_log ( migration_type, migration_version ) VALUES( 0, 1 );
-
 create table domains (
- id              SERIAL PRIMARY KEY,
+ id              SERIAL PRIMARY KEY NOT NULL,
  name            VARCHAR(255) NOT NULL,
  master          VARCHAR(128) DEFAULT NULL,
  last_check      INT DEFAULT NULL,
@@ -32,7 +24,7 @@ create table domains (
 CREATE UNIQUE INDEX name_index ON domains(name);
 
 CREATE TABLE records (
-        id              SERIAL PRIMARY KEY,
+        id              SERIAL PRIMARY KEY NOT NULL,
         domain_id       INT DEFAULT NULL,
         name            VARCHAR(255) DEFAULT NULL,
         type            VARCHAR(10) DEFAULT NULL,
@@ -59,7 +51,7 @@ create table supermasters (
 );
 
 CREATE TABLE comments (
-  id              SERIAL PRIMARY KEY,
+  id              SERIAL PRIMARY KEY NOT NULL,
   domain_id       INT NOT NULL,
   name            VARCHAR(255) NOT NULL,
   type            VARCHAR(10) NOT NULL,
@@ -74,4 +66,3 @@ CREATE TABLE comments (
 CREATE INDEX comments_domain_id_idx ON comments (domain_id);
 CREATE INDEX comments_name_type_idx ON comments (name, type);
 CREATE INDEX comments_order_idx ON comments (domain_id, modified_at);
-END;
